476bb2d9ee3d8ce47dfb286ae05984621045e5e5
Emir <itsmohebbi@gmail.com>
2025-02-24 12:36:09 +0330

    add the css to project

--- src/components/SideBar..jsx
+++ src/components/SideBar..jsx
@@ -1,7 +1,16 @@
 import { FaListUl } from "react-icons/fa";
 import { createQueryObject } from "../helper/helper";
+import styles from "./SideBar.module.css";
 
-function SideBar({ setQuery }) {
+const categories = [
+  { id: 1, type: "All" },
+  { id: 2, type: "Electronics" },
+  { id: 3, type: "Jewelery" },
+  { id: 4, type: "Men's Clothing" },
+  { id: 5, type: "Women's Clothing" },
+];
+
+function SideBar({ query, setQuery }) {
   const categoryHandeler = (e) => {
     const { tagName } = e.target;
     const category = e.target.innerText.toLowerCase();
@@ -11,17 +20,24 @@ function SideBar({ setQuery }) {
   };
   return (
     <>
-      <div>
+      <div className={styles.sidebar}>
         <div>
           <FaListUl />
           <p>Categories</p>
         </div>
         <ul onClick={categoryHandeler}>
-          <li>All</li>
-          <li>Electronics</li>
-          <li>Jewelery</li>
-          <li>Men's clothing</li>
-          <li>Women's clothing</li>
+          {categories.map((item) => (
+            <li
+              key={item.id}
+              className={
+                item.type.toLowerCase() === query.category
+                  ? styles.selected
+                  : null
+              }
+            >
+              {item.type}
+            </li>
+          ))}
         </ul>
       </div>
     </>
